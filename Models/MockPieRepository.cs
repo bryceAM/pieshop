127f5bfe4ae6b41cc7edbc0ecf6340e415cfc23b
Bryce <bryce.malmberg@gmail.com>
2024-12-11 23:38:35 -0600

    feat 1: models

---
+++ Models/MockPieRepository.cs
@@ -0,0 +1,48 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        //IEnumerable<Pie> AllPies { get; }
+        //IEnumerable<Pie> PiesOfTheWeek { get; set; }
+        //Pie? GetPieById(int pieId);
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+        public IEnumerable<Pie> AllPies => 
+            new List<Pie>
+            {
+                new Pie { PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription="Lorem Ipsum", LongDescription="sdlfkjsdl;fjs sdf;lasjdfl; sldk;jfsdlkfjds;l.", Category = _categoryRepository.AllCategories.ToList()[0], ImageUrl="https://gillcleerenpluralsight.blob.core.windows.net/files/bethanyspieshop/fruitpies/strawberrypie.jpg", InStock=true,IsPieOfTheWeek=false, ImageThumbnailUrl="https://"}
+
+                //        public int PieId { get; set; }
+                //public string Name { get; set; } = string.Empty;
+                //public string? ShortDescription { get; set; }
+                //public string? LongDescription { get; set; }
+                //public string? AllergyInformation { get; set; }
+                //public decimal Price { get; set; }
+                //public string? ImageUrl { get; set; }
+                //public string? ImageThumbnailUrl { get; set; }
+                //public bool IsPieOfTheWeek { get; set; }
+                //public bool InStock { get; set; }
+                //public int CategoryId { get; set; }
+                //public Category Category { get; set; } = default!;
+            };
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return AllPies.Where(p => p.IsPieOfTheWeek);
+            }
+
+            set
+            {
+
+            }
+        }
+
+        public Pie? GetPieById(int pieId) => AllPies.FirstOrDefault(p => p.PieId == pieId);
+
+        public IEnumerable<Pie> SearchPies(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
