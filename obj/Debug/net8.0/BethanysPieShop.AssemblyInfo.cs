ec9749eb5d8417887802862611bb967a2545f046
Bryce <bryce.malmberg@gmail.com>
2024-12-25 00:48:04 -0600

    feat 16: add tag helpers

--- obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216c8481e900a2218f708d4069723118c7685dc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2057f073e4cb805aeb25279ca80335d260de1018")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
