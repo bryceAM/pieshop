087d176de8d9ad613a93d4e97a22abe0e86cdf45
Bryce <bryce.malmberg@gmail.com>
2025-01-02 22:11:47 -0600

    feat 18: add order classes and interface and dbsets in preparation for the Order form feature

--- obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9749eb5d8417887802862611bb967a2545f046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a929a656f6089d7abb8995a948cd9af4517e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
