de464f5cebefc4fac3833ba30921eab7a96ba61d
Bryce <bryce.malmberg@gmail.com>
2025-01-05 21:35:36 -0600

    feat 20: order controller's model binding

--- obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+087d176de8d9ad613a93d4e97a22abe0e86cdf45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c2f7a8f14e55800d60ba446be10e244c26cc47")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
