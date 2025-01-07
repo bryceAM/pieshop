aeb509352431fc7cf08bb8d6ed22beacfc9aa4b1
Bryce <bryce.malmberg@gmail.com>
2025-01-06 22:04:28 -0600

    feat 23: add razor pages CheckoutPage and CheckoutCompletePage

--- obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3deafe555c19394b340cdd64e8e72460efa052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a87d7aaa5c5b8ec8353af2bdee5953dd6a417227")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
