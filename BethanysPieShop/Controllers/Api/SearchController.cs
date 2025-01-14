17e87a4b3a244e944992bd268993e3f1df12b0f3
Bryce <bryce.malmberg@gmail.com>
2025-01-13 21:40:59 -0600

    feat 27: initialize the searchController API

---
+++ BethanysPieShop/Controllers/Api/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers.Api
+{
+    // ASP.NET will replace [controller] with Search, since this is in the file SearchController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public SearchController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+
+        }
+    }
+}
