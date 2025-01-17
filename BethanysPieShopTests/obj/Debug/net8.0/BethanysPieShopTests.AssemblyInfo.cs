c30a9fd9741bf2b6d7e9fc668ce7ea1083d514d5
Bryce <bryce.malmberg@gmail.com>
2025-01-16 22:10:36 -0600

    feat 31: prepare the app for Identity

--- BethanysPieShopTests/obj/Debug/net8.0/BethanysPieShopTests.AssemblyInfo.cs
+++ BethanysPieShopTests/obj/Debug/net8.0/BethanysPieShopTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e969c8d256828241f64a4e61a7ea43a8170f209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46033e132ef9f9831978347f26c6d949344f5850")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
